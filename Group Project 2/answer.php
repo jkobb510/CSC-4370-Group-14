de238e81804706497cdb4fda7d995102bee3af3e
Chowdhury Mobin <55259583+ChowdhuryMobin@users.noreply.github.com>
2020-10-31 13:15:19 -0400

    Add files via upload

---
+++ Group Project 2/answer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+   <title>Score</title>
+</head>
+<body>
+
+   <div class="container">
+
+      <h1>Your Result </h1>
+
+      <?php
+
+         $answer1 = $_POST['question-1-answers'];
+
+         $totalscore = 0;
+
+         if ($answer1 == "A") {$totalscore++;}
+
+         if ($answer1 == "B") {$totalscore--;}
+
+         if ($answer1 == "C") {$totalscore--;}
+
+         if ($answer1 == "D") {$totalscore--;}
+     
+         echo "<div id='result'> Score: $totalscore</div>";
+
+      ?>
+
+   </div>
+   
+</body>
+</html>
