ee7d53cc1182f5c05e8c3eb2e4677271e9787489
Chowdhury Mobin <55259583+ChowdhuryMobin@users.noreply.github.com>
2020-10-31 13:33:19 -0400

    Question/Score

--- Group Project 2/answer.php
+++ Group Project 2/answer.php
@@ -1,36 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="style.css">
    <title>Score</title>
 </head>
+
 <body>
 
    <div class="container">
 
-      <h1>Your Result </h1>
+      <h1>Your Result</h1>
 
       <?php
 
-         $answer1 = $_POST['question-1-answers'];
+      $answer1 = $_POST['question-1-answers'];
 
-         $totalscore = 0;
+      $totalscore = 0;
 
-         if ($answer1 == "A") {$totalscore++;}
+      if ($answer1 == "A") {
+         $totalscore++;
+      }
 
-         if ($answer1 == "B") {$totalscore--;}
+      if ($answer1 == "B") {
+         $totalscore--;
+      }
 
-         if ($answer1 == "C") {$totalscore--;}
+      if ($answer1 == "C") {
+         $totalscore--;
+      }
 
-         if ($answer1 == "D") {$totalscore--;}
-     
-         echo "<div id='result'> Score: $totalscore</div>";
+      if ($answer1 == "D") {
+         $totalscore--;
+      }
+
+      echo "<div class='score'> Score: $totalscore</div>";
+
+      echo "<div class='button'> <a href='homepage.html'> Return home </a> </div>";
 
       ?>
 
    </div>
-   
+
 </body>
+
 </html>
