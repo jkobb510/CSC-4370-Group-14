324777141f2228009207827b00b427a4b9026bd4
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 21:56:14 -0500

    Update homepage.php

--- Group Project 2/homepage.php
+++ Group Project 2/homepage.php
@@ -14,36 +14,37 @@
    <h1><img src="photos/jep_logo.png" alt="jep logo" style="width: 250px"></h1>
    <div class="container">
 
-      <div class="box">Category 1</div>
-      <div class="box">Category 2</div>
-      <div class="box">Category 3</div>
-      <div class="box">Category 4</div>
-      <div class="box">Category 5</div>
-      <div class="box"> <a href="question.php"> $100</a></div>
-      <div class="box">$100</div>
-      <div class="box">$100</div>
-      <div class="box">$100</div>
-      <div class="box">$100</div>
-      <div class="box">$200</div>
-      <div class="box">$200</div>
-      <div class="box">$200</div>
-      <div class="box">$200</div>
-      <div class="box">$200</div>
-      <div class="box">$300</div>
-      <div class="box">$300</div>
-      <div class="box">$300</div>
-      <div class="box">$300</div>
-      <div class="box">$300</div>
-      <div class="box">$400</div>
-      <div class="box">$400</div>
-      <div class="box">$400</div>
-      <div class="box">$400</div>
-      <div class="box">$400</div>
-      <div class="box">$500</div>
-      <div class="box">$500</div>
-      <div class="box">$500</div>
-      <div class="box">$500</div>
-      <div class="box">$500</div>
+
+      <div class="box">Technology</div>
+      <div class="box">History</div>
+      <div class="box">Pop Culture</div>
+      <div class="box">Science</div>
+      <div class="box">Misc.</div>
+      <div class="box"> <a href="question.html"> $100</a></div>
+      <div class="box"><a href="question6.html">$100 </a></div>
+      <div class="box"><a href="question11.html">$100 </a></div>
+      <div class="box"><a href="cell.html">$100 </a></div>
+      <div class="box"><a href="misc1.html">$100 </a></div>
+      <div class="box"><a href="question2.html"> $200</a></div>
+      <div class="box"><a href="question7.html">$200</a></div>
+      <div class="box"><a href="dicaprio_Q.html">$200</a></div>
+      <div class="box"><a href="waves.html">$200</a></div>
+      <div class="box"><a href="misc2.html">$200</a></div>
+      <div class="box"><a href="question3.html">$300</a></div>
+      <div class="box"><a href="question8.html">$300</a></div>
+      <div class="box"><a href="foreign.html">$300</a></div>
+      <div class="box"><a href="bond.html">$300</a></div>
+      <div class="box"><a href="misc3.html">$300</a></div>
+      <div class="box"><a href="question4.html">$400</a></div>
+      <div class="box"><a href="question9.html">$400</a></div>
+      <div class="box"><a href="marriagestory_q.html">$400</a></div>
+      <div class="box"><a href="medicine.html">$400</a></div>
+      <div class="box"><a href="misc4.html">$400</a></div>
+      <div class="box"><a href="question5.html">$500</a></div>
+      <div class="box"><a href="question10.html">$500</a></div>
+      <div class="box"><a href="musk_q.html">$500</a></div>
+      <div class="box"><a href="keto.html">$500</a></div>
+      <div class="box"><a href="misc5.html">$500</a></div>
 
    </div>
    <div>
