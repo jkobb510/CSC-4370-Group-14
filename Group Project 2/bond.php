1618573f21b0749ce6e07df4d0e303cf8f517568
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 22:18:04 -0500

    Create bond.php

---
+++ Group Project 2/bond.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Question</title>
+</head>
+
+<body>
+   <?php session_start(); ?>
+   <form action="answer.php" method="POST" class="container">
+      <div class="title"> Category: Science </div>
+       
+
+      <div id="question" class="question"> The phrase "sharing is caring" is associated with this chemical bond, referring its sharing of electron pairs. </div>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="A" />radio waves <span
+            id="question-answers-A">Covalent Bond</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="B" /> infrared waves<span
+            id="question-answers-B">Ionic Bond</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="C" />microwaves <span
+            id="question-answers-C">Hydrogen Bond</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="D" />All of the above <span
+            id="question-answers-D">Equal Bond</span></label>
+
+      <input class="button" type="submit" value="Submit">
+
+</form>
+
+   <?php 
+   echo $_SESSION["username"];
+   echo $_SESSION["points"];
+    ?>
+</body>
+
+</html>
