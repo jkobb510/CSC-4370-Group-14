91f69a7b8ebaf16575462f120875b8e1272380d6
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 22:12:54 -0500

    Create marriagestory_q.php

---
+++ Group Project 2/marriagestory_q.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Question #1</title>
+</head>
+
+<body>
+   <?php session_start(); ?>
+   <form action="answer.php" method="POST" class="container">
+
+      <div class="title"> Category: Pop Culture </div>
+       
+
+      <div id="question" class="question"> Scarlett Johansson and this actor starred in the film "Marriage Story" </div>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="A" /> <span
+            id="question-answers-A">Mark Wahlberg</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+            id="question-answers-B">Keanu Reeves </span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+            id="question-answers-C">Adam Driver</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+            id="question-answers-D">Matt Damon</span></label>
+
+      <input class="button" type="submit" value="Submit">
+
+</form>
+
+   <?php 
+   echo $_SESSION["username"];
+   echo $_SESSION["points"];
+    ?>
+</body>
+
+</html>
