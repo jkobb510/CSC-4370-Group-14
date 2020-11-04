3cb4a33b61ace74371b158df1137e236a000e1be
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 22:20:49 -0500

    Create keto.php

---
+++ Group Project 2/keto.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Question</title>
+</head>
+
+<body>
+   <?php session_start(); ?>
+   <form action="answer.php" method="POST" class="container">
+
+      <div class="title"> Category: Science </div>
+       
+
+      <div id="question" class="question">The ketogenic diet aims to switch the body's energy source from ____ to ____ </div>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="A" />radio waves <span
+            id="question-answers-A"></span>Fat, Glucose</label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="B" /> infrared waves<span
+            id="question-answers-B"></span>Glucose, Fat</label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="C" />microwaves <span
+            id="question-answers-C"></span>Fatty Acids, Cholesterol</label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="D" />All of the above <span
+            id="question-answers-D"></span> Calories, ATP</label>
+
+      <input class="button" type="submit" value="Submit">
+</form>
+
+   <?php 
+   echo $_SESSION["username"];
+   echo $_SESSION["points"];
+    ?>
+</body>
+
+</html>
