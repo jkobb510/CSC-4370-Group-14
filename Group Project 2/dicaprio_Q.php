df7a89d81a4a60adf8ab3fdc8da75f3e76afef29
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 22:11:02 -0500

    Create dicaprio_Q.php

---
+++ Group Project 2/dicaprio_Q.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Question #1</title>
+</head>
+
+<body>
+   <?php session_start(); ?>
+   <form action="answer.php" method="POST" class="container">
+
+      <div class="title"> Category: Pop Culture </div>
+
+      <div id="question" class="question"> The Dicaprio image below is used for memes based on what movie? </div>
+       <img src="photos/Dicaprio.png" alt="Dicaprio meme">
+
+      <label class="option"><input type="radio" name="question-9-answers" value="A" /> <span
+            id="question-answers-A">The Revenant</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+            id="question-answers-B">Wolf of Wall Street</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+            id="question-answers-C">Django Unchained</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+            id="question-answers-D">Gangs of New York</span></label>
+
+      <input class="button" type="submit" value="Submit">
+</form>
+
+   <?php 
+   echo $_SESSION["username"];
+   echo $_SESSION["points"];
+    ?>
+</body>
+
+</html>
