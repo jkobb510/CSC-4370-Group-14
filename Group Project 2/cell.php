2f3b0f55663249c6e6708e2beb836064139d5931
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-03 22:17:13 -0500

    Create cell.php

---
+++ Group Project 2/cell.php
@@ -0,0 +1,43 @@
+
+      
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Question</title>
+</head>
+
+<body>
+   <?php session_start(); ?>
+   <form action="answer.php" method="POST" class="container">
+      <div class="title"> Category: Science </div>
+       
+
+      <div id="question" class="question"> This organelle is known as the powerhouse of a cell  </div>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="A" /> <span
+            id="question-answers-A">Nucleus</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+            id="question-answers-B">Mitochondria</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+            id="question-answers-C">ER</span></label>
+
+      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+            id="question-answers-D">Cytoplasm</span></label>
+
+      <input class="button" type="submit" value="Submit">
+
+</form>
+
+   <?php 
+   echo $_SESSION["username"];
+   echo $_SESSION["points"];
+    ?>
+</body>
+
+</html>
