c02191561dd7db09b329629734b1d044cc8339b8
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:20:26 -0500

    Update question6.php

--- Group Project 2/question6.php
+++ Group Project 2/question6.php
@@ -16,16 +16,16 @@
 
       <div id="question" class="question"> Abraham Lincoln was the ___ U.S. president. </div>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-answers-A">16th</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-answers-B">14th</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-answers-C">15th</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-answers-D">23rd</span></label>
 
       <input class="button" type="submit" value="Submit">
