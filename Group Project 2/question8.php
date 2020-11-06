10b0c22328241550511bed51a9074c714337589a
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:22:39 -0500

    Update question8.php

--- Group Project 2/question8.php
+++ Group Project 2/question8.php
@@ -6,7 +6,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="css/style.css">
-   <title>Question #1</title>
+   <title>Question 8</title>
 </head>
 
 <body>
@@ -16,16 +16,16 @@
 
       <div id="question" class="question"> Who invented the practical telephone? </div>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-answers-A">Alexander Graham Bell</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="questio-answers" value="B" /> <span
             id="question-answers-B">Thomas Edison</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-answers-C">Benjamin Franklin</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-answers-D">Nikola Tesla</span></label>
 
       <input class="button" type="submit" value="Submit">
