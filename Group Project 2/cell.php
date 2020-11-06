e30766b4a29270f5995aad96707cd17bb9fc09cf
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 16:51:01 -0500

    Update cell.php

--- Group Project 2/cell.php
+++ Group Project 2/cell.php
@@ -18,16 +18,16 @@
 
       <div id="question" class="question"> This organelle is known as the powerhouse of a cell  </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-answers-A">Nucleus</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-answers-B">Mitochondria</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-answers-C">ER</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-answers-D">Cytoplasm</span></label>
 
       <input class="button" type="submit" value="Submit">
