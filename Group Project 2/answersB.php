f896ee9278bd0daa1a82c2d9d79e23056c043aab
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 16:59:52 -0500

    Add files via upload

---
+++ Group Project 2/answersB.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Score</title>
+</head>
+
+<body>
+
+   <div class="container">
+
+      <h1><img src="photos/jep_logo.png" alt="jep logo" style="width: 250px"></h1>
+
+      <?php
+      session_start();
+
+      $answer = $_POST['question-answers'];
+
+      $totalscore = 0;
+
+      if ($answer == "B") { $_SESSION["points"] += 1; }
+      else { $_SESSION["points"] -= 1; }
+
+      echo "<div class='score'> Score: {$_SESSION["points"]}</div>";
+
+      echo "<div class='button1'> <a href='homepage.php'> Return home </a> </div>";
+
+      ?>
+
+   </div>
+
+</body>
+
+</html>
