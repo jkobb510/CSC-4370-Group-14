397beb6005510a3ea70cb362b64162517c61456b
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:23:16 -0500

    Update question9.php

--- Group Project 2/question9.php
+++ Group Project 2/question9.php
@@ -6,27 +6,27 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="css/style.css">
-   <title>Question #1</title>
+   <title>Question 9</title>
 </head>
 
 <body>
    <?php session_start(); ?>
-   <form action="answer.php" method="POST" class="container">
+   <form action="answersD.php" method="POST" class="container">
 
       <div class="title"> Category: History </div>
 
       <div id="question" class="question"> What war was coined the term "The Forgotten War"? </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-9-answers-A">War of 1812</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-9-answers-B">War of Jenkins' Ear</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-9-answers-C">The Russo-Japanese War</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-9-answers-D">The Korean War</span></label>
 
       <input class="button" type="submit" value="Submit">
