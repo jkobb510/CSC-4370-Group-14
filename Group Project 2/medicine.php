ae0b52afce983071319104f7588bc8b249ed8f02
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:56:18 -0500

    Update medicine.php

--- Group Project 2/medicine.php
+++ Group Project 2/medicine.php
@@ -11,24 +11,24 @@
 
 <body>
    <?php session_start(); ?>
-   <form action="answer.php" method="POST" class="container">
+   <form action="answersC.php" method="POST" class="container">
 
       <div class="title"> Category: Science </div>
        
 
       <div id="question" class="question">The discovery of this drug changed the course medicine as it was the first effective drug to fight off infection. </div>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="A" />radio waves <span
-            id="question-answers-A"></span>Azithromycin (Zithromax/Z-Pak)</label>
+      <label class="option"><input type="radio" name="question-answers" value="A" />radio waves <span
+            id="question-answers-A"></span>Aspirin drug</label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="B" /> infrared waves<span
-            id="question-answers-B">Acetaminophen (Tylenol)</span></label>
+      <label class="option"><input type="radio" name="question-answers" value="B" /> infrared waves<span
+            id="question-answers-B">Tylenol</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="C" />microwaves <span
+      <label class="option"><input type="radio" name="question-answers" value="C" />microwaves <span
             id="question-answers-C">Penicillin</span></label>
 
-      <label class="option"><input type="radio" name="question-9-answers" value="D" />All of the above <span
-            id="question-answers-D">NSAIDs</span></label>
+      <label class="option"><input type="radio" name="question-answers" value="D" />All of the above <span
+            id="question-answers-D">Advil</span></label>
 
       <input class="button" type="submit" value="Submit">
 </form>
