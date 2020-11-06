7da9b2f4f797be6d874942821cd757c7c418cf62
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:11:46 -0500

    Update question3.php

--- Group Project 2/question3.php
+++ Group Project 2/question3.php
@@ -6,7 +6,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="css/style.css">
-   <title>Question #1</title>
+   <title>Question 3</title>
 </head>
 
 <body>
@@ -16,16 +16,16 @@
 
       <div id="question" class="question"> Short for WebLog – an online journal that is frequently updated </div>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-1-answers-A">Blog</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-1-answers-B">Thread</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-1-answers-C">Interactive Journalism</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-1-answers-D">Think Tank</span></label>
 
       <input class="button" type="submit" value="Submit">
