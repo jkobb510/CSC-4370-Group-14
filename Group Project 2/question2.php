f327b12cba89b188afdb6f17a810e79d7c9bf40c
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-11-06 17:10:24 -0500

    Update question2.php

--- Group Project 2/question2.php
+++ Group Project 2/question2.php
@@ -10,21 +10,21 @@
 
 <body>
    <?php session_start(); ?>
-   <form action="answer.php" method="POST" class="container">   
+   <form action="answersB.php" method="POST" class="container">   
     <div class="title"> Category: "Technology" </div>
 
       <div id="question" class="question"> In 1989, the World Wide Web was invented by this person.  </div>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="A" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="A" /> <span
             id="question-answers-A">Al Gore</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="B" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="B" /> <span
             id="question-answers-B">Tim Berners-Lee</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="C" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="C" /> <span
             id="question-answers-C">Bob Kahn</span></label>
 
-      <label class="option"><input type="radio" name="question-1-answers" value="D" /> <span
+      <label class="option"><input type="radio" name="question-answers" value="D" /> <span
             id="question-answers-D">Bill Gates</span></label>
 
       <input class="button" type="submit" value="Submit">
